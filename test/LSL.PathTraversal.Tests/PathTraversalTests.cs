6266407a207e5a1b75dfe250eaaee24f829bd4d9
Alun Jones <alunacjones@gmail.com>
2025-05-24 10:41:24 +0100

    just another test to play iwth object merging to a set level

--- test/LSL.PathTraversal.Tests/PathTraversalTests.cs
+++ test/LSL.PathTraversal.Tests/PathTraversalTests.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Diamond.Core.System.TemporaryFolder;
 using FluentAssertions;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using DC = LSL.DisposableCurrentDirectory;
 
 namespace LSL.PathTraversal.Tests;
 
@@ -52,7 +57,7 @@ public class PathTraversalTests
             .Should()
             .NotBeNull();
     }
-    
+
     [Test]
     public void Enumerate_WithAValidPath_AndMatchingANullSingleFile_ShouldThrowTheExpectedException()
     {
@@ -69,5 +74,46 @@ public class PathTraversalTests
             .ParamName
             .Should()
             .Be("fileName");
-    }    
+    }
+
+    [Test]
+    public async Task Enumerate_Merging()
+    {
+        using var currentFolder = new DC.DisposableCurrentDirectory("TestData/Merging/inner1/inner2");
+
+        var files = PathTraversal.Enumerate()
+            .Select(di => di.GetFileInfo("test.json"))
+            .TakeWhile(fi => fi.Directory.Name != "TestData")
+            .Reverse();
+
+        var startValue = JObject.Parse("{}");
+
+        foreach (var file in files)
+        {
+            var content = await File.ReadAllTextAsync(file.FullName);
+            startValue.Merge(JObject.Parse(content), new() { MergeArrayHandling = MergeArrayHandling.Replace });
+        }
+
+        startValue.ToObject<MergeData>()
+            .Should()
+            .BeEquivalentTo(new MergeData
+            {
+                Integer = 99,
+                String = "inner1-string",
+                StringArray = [
+                    "inner2-item1",
+                    "inner2-item2",
+                    "inner2-item3"
+                ]
+            });
+            
+        files.Should().HaveCount(3);
+    }
+
+    private class MergeData
+    {
+        public int Integer { get; set; }
+        public string String { get; set; }
+        public IEnumerable<string> StringArray { get; set; } = [];
+    }
 }
